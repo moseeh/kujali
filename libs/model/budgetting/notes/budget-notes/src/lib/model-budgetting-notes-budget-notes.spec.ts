3278d9d2c092676f17146d5f2de52e06e7eaf59a
moonyango <mosesadrian825@gmail.com>
2025-11-29 14:50:20 +0300

    feat: nx boilerplate files

---
+++ libs/model/budgetting/notes/budget-notes/src/lib/model-budgetting-notes-budget-notes.spec.ts
@@ -0,0 +1,9 @@
+import { modelBudgettingNotesBudgetNotes } from './model-budgetting-notes-budget-notes';
+
+describe('modelBudgettingNotesBudgetNotes', () => {
+  it('should work', () => {
+    expect(modelBudgettingNotesBudgetNotes()).toEqual(
+      'modelwhy-budgetting-notes-budget-notes'
+    );
+  });
+});
